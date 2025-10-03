621647d8176dd3bbc69ffb99c101f16ecc5542c0
Joppe2003 <564285@edu.rocmn.nl>
2025-10-03 09:58:47 +0200

    Feature UC13: - Onnodige unit tests weg gehaald die niet use case horen

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -21,43 +21,6 @@ namespace TestCore
         {
         }
 
-
-        //Happy flow
-        [Test]
-        public void TestPasswordHelperReturnsTrue()
-        {
-            string password = "user3";
-            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
-            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
-        }
-
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")]
-        public void TestPasswordHelperReturnsTrue(string password, string passwordHash)
-        {
-            Assert.IsTrue(PasswordHelper.VerifyPassword(password, passwordHash));
-        }
-
-
-        //Unhappy flow
-        [Test]
-        public void TestPasswordHelperReturnsFalse()
-        {
-            string password = "wrongpassword";
-            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
-
-
-
-            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash)); //Zelf uitwerken
-        }
-
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
-        {
-            Assert.IsFalse(PasswordHelper.VerifyPassword(password, password)); //Zelf uitwerken zodat de test slaagt!
-        }
-
         [Test]
         public void TestOverviewProductInGrocyListReturnsTrue()
         {
